2b997092563554e2e6db8d9a10d82349000834d5
zhuaoqi <1561878031@163.com>
2018-09-13 18:18:48 +0800

    first

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 //app.js
+var hexMD5 = require('utils/md5.js')
+var request = require('utils/request.js')
+
 App({
+  
   onLaunch: function () {
     // 展示本地存储能力
+    console.log(1)
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res)
+        request.request('login.login.login', { code: res.code, type: 2 }, res => {
+          console.log(res)
+        }, err => {
+          console.log(err)
+        })
       }
     })
     // 获取用户信息
@@ -17,21 +28,13 @@ App({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+          
         }
       }
     })
+  },
+  RequiseData(methods, data, callback, errFun) {
+    request.request(methods, data, callback, errFun)
   },
   globalData: {
     userInfo: null
---
+++ utils/request.js
@@ -0,0 +1,72 @@
+var md5 = require('md5.js')
+var api_url = 'https://test.gtdysd.com/Rest/business/';
+var appid = 1000;
+var _timestamp = Math.round(new Date().getTime() / 1000).toString();
+function appRequest(methods, data, callback, errFun) {
+  var data = data;
+  var sign = api(data, methods);
+  var keyname = '';
+  data.v = '1.0'
+  data.method = methods
+  data.appid = appid
+  data.uuid = 'webseaver_crm'
+  data.platform = 'web'
+  data._timestamp = _timestamp
+  data.sign = sign
+  Object.keys(data).forEach(function (key) {
+    keyname += key + '=' + data[key] + '&';
+  });
+  console.log(keyname)
+  wx.request({
+    url: api_url, // api接口地址请求
+    method: 'POST', //
+    header: {
+      'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    dataType: 'json',
+    data: keyname,
+    success: function (res) {
+      callback(res.data);
+    },
+    fail: function (err) {
+      errFun(err);
+    }
+  })
+}
+function api(data, method){
+  var request_data = [];
+  request_data['v'] = '1.0';
+  request_data['appid'] = appid;
+  request_data['method'] = method;
+  request_data['uuid'] = "webseaver_crm";
+  request_data['platform'] = "web";
+  request_data['_timestamp'] = _timestamp;
+  for (var d in data) {
+    request_data[d] = data[d];
+  }
+  return signature(request_data);
+}
+function objKeySort(obj) { // 排序的函数
+  var newkey = Object.keys(obj).sort();
+  var newObj = {}; // 创建一个新的对象，用于存放排好序的键值对
+  for (var i = 0; i < newkey.length; i++) { // 遍历newkey数组
+    newObj[newkey[i]] = obj[newkey[i]]; // 向新创建的对象中按照排好的顺序依次增加键值对
+  }
+  return newObj; // 返回排好序的新对象
+}
+function signature(data) {
+  var client_secret = 'ca9c2e74e96c3d65c5b956f0770194ca';
+  var ret = [];
+  ret = objKeySort(data)
+  var sign_str = "";
+  for (var d in ret) {
+    var k = d;
+    var v = ret[d];
+    sign_str += k + v
+  }
+  sign_str += client_secret
+  return md5.hexMD5(sign_str)
+}
+module.exports = {
+  request: appRequest
+}  
